7f444181c55d5f41d1eb656fe1384a2ba1e379ae
Alireza <orginalireza@gmail.com>
2023-07-27 14:07:55 +0330

    add git ignore

--- test.php
+++ test.php
@@ -20,7 +20,7 @@ $data = array(
 #init curl with this : 
 $request = new CurlReq();
 # To set url use setUrl("your_url") : 
-$request->setUrl("https://httpbin.org/delete");
+$request->setUrl("https://httpbin.org/post");
 
 #if you your requestis json call data will json encoded and json headers add: 
 $request->json();
@@ -29,35 +29,14 @@ $request->json();
 $request->setData($data);
 
 
-#add your headers by call setHeaders(): 
-$request->setHeaders($your_array_headers);
-
-#change your request time out by call setTimeOut() in Second: 
-$request->setTimeOut($Second);
-
-#you can trun Exception on or off by call Exception(false or true)
-#on true exception will throw and on false will not default is false : 
-$request->Exception(false); # exception off
-$request->Exception(true); # exception on
-
-#use get,post,put,delete method base on url accepted method
-$request->get();
-$request->post();
-$request->put();
-$request->delete();
 
 
-
-$request->response_headers;
-$request->response;
-$request->errors;
-$request->error_number;
 
 // $x->setUrl("https://httpbin.org/post")->json()->setData($data);
 // $x->setUrl("https://httpbin.org/get")->json()->setData($data);
 // var_dump($x);
-$x->delete();
+$request->delete();
 // $x->sendGet();
 
-var_dump($x->response_headers);
-var_dump($x->response);
+// var_dump($x->response_headers);
+var_dump($x->responseInArray());
